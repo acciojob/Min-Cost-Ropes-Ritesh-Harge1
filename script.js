81acd2b0165f0ed2d75dd7a82c55ed9298536d41
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-26 10:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,77 +1,89 @@
+// Min Cost Ropes using Min-Heap (Priority Queue)
+
+// Priority Queue Implementation
 class MinHeap {
   constructor() {
     this.heap = [];
   }
-  getLeftChildIndex(i) { return 2 * i + 1; }
-  getRightChildIndex(i) { return 2 * i + 2; }
-  getParentIndex(i) { return Math.floor((i - 1) / 2); }
-  swap(i, j) { [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]]; }
 
-  insert(key) {
-    this.heap.push(key);
-    this.heapifyUp();
+  // Swap utility
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
   }
-  heapifyUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      let parentIndex = this.getParentIndex(index);
-      if (this.heap[parentIndex] > this.heap[index]) {
-        this.swap(parentIndex, index);
-        index = parentIndex;
-      } else break;
+
+  // Insert value into heap
+  push(val) {
+    this.heap.push(val);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  // Heapify upwards
+  bubbleUp(index) {
+    let parent = Math.floor((index - 1) / 2);
+    while (index > 0 && this.heap[parent] > this.heap[index]) {
+      this.swap(parent, index);
+      index = parent;
+      parent = Math.floor((index - 1) / 2);
     }
   }
-  extractMin() {
-    if (this.heap.length === 0) return null;
+
+  // Remove min element
+  pop() {
     if (this.heap.length === 1) return this.heap.pop();
-    const root = this.heap[0];
+    const min = this.heap[0];
     this.heap[0] = this.heap.pop();
-    this.heapifyDown();
-    return root;
+    this.bubbleDown(0);
+    return min;
   }
-  heapifyDown() {
-    let index = 0;
-    while (this.getLeftChildIndex(index) < this.heap.length) {
-      let smallerChildIndex = this.getLeftChildIndex(index);
-      let rightChildIndex = this.getRightChildIndex(index);
-      if (rightChildIndex < this.heap.length &&
-          this.heap[rightChildIndex] < this.heap[smallerChildIndex]) {
-        smallerChildIndex = rightChildIndex;
-      }
-      if (this.heap[index] > this.heap[smallerChildIndex]) {
-        this.swap(index, smallerChildIndex);
-        index = smallerChildIndex;
-      } else break;
+
+  // Heapify downwards
+  bubbleDown(index) {
+    let left = index * 2 + 1;
+    let right = index * 2 + 2;
+    let smallest = index;
+
+    if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
+      smallest = left;
+    }
+    if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
+      smallest = right;
+    }
+    if (smallest !== index) {
+      this.swap(index, smallest);
+      this.bubbleDown(smallest);
     }
   }
-  size() { return this.heap.length; }
+
+  size() {
+    return this.heap.length;
+  }
 }
 
+// Main function
 function mincost(arr) {
-  if (arr.length <= 1) return 0;
   let heap = new MinHeap();
-  for (let num of arr) heap.insert(num);
-  let totalCost = 0;
-  while (heap.size() > 1) {
-    let first = heap.extractMin();
-    let second = heap.extractMin();
-    let newRope = first + second;
-    totalCost += newRope;
-    heap.insert(newRope);
+
+  // Insert all rope lengths into heap
+  for (let rope of arr) {
+    heap.push(rope);
   }
+
+  let totalCost = 0;
+
+  // Keep connecting ropes until one remains
+  while (heap.size() > 1) {
+    let first = heap.pop();
+    let second = heap.pop();
+    let cost = first + second;
+    totalCost += cost;
+    heap.push(cost);
+  }
+
   return totalCost;
 }
 
-// ✅ Reading user input
-const readline = require("readline");
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+// Examples
+console.log(mincost([4, 3, 2, 6])); // 29
+console.log(mincost([1, 2, 3, 4, 5])); // 33
 
-let input = [];
-rl.on("line", (line) => {
-  input.push(line.trim());
-}).on("close", () => {
-  // assume input is space-separated integers on one line
-  let arr = input[0].split(" ").map(Number);
-  console.log(mincost(arr));
-});
 
