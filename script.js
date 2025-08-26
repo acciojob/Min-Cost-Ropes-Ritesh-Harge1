01af90eb514c1be7ac66283ca816496598301022
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-26 09:41:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,87 @@
-function mincost(arr) {
-  if (arr.length <= 1) return 0; // No cost if only one rope
-
-  let cost = 0;
-
-  // Convert to array and sort
-  arr.sort((a, b) => a - b);
-
-  while (arr.length > 1) {
-    // Take two smallest ropes
-    let first = arr.shift();
-    let second = arr.shift();
-
-    let newRope = first + second;
-    cost += newRope;
-
-    // Insert back into sorted order
-    arr.push(newRope);
-    arr.sort((a, b) => a - b);
+class MinHeap {
+  constructor() {
+    this.heap = [];
   }
 
-  return cost;
+  getLeftChildIndex(i) { return 2 * i + 1; }
+  getRightChildIndex(i) { return 2 * i + 2; }
+  getParentIndex(i) { return Math.floor((i - 1) / 2); }
+
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  insert(key) {
+    this.heap.push(key);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parentIndex = this.getParentIndex(index);
+      if (this.heap[parentIndex] > this.heap[index]) {
+        this.swap(parentIndex, index);
+        index = parentIndex;
+      } else break;
+    }
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      let rightChildIndex = this.getRightChildIndex(index);
+
+      if (rightChildIndex < this.heap.length &&
+          this.heap[rightChildIndex] < this.heap[smallerChildIndex]) {
+        smallerChildIndex = rightChildIndex;
+      }
+
+      if (this.heap[index] > this.heap[smallerChildIndex]) {
+        this.swap(index, smallerChildIndex);
+        index = smallerChildIndex;
+      } else break;
+    }
+  }
+
+  size() { return this.heap.length; }
 }
 
-// Test cases
+function mincost(arr) {
+  if (arr.length <= 1) return 0;
+
+  let heap = new MinHeap();
+  for (let num of arr) {
+    heap.insert(num);
+  }
+
+  let totalCost = 0;
+
+  while (heap.size() > 1) {
+    let first = heap.extractMin();
+    let second = heap.extractMin();
+
+    let newRope = first + second;
+    totalCost += newRope;
+
+    heap.insert(newRope);
+  }
+
+  return totalCost;
+}
+
+// ✅ Test cases
 console.log(mincost([4, 3, 2, 6])); // 29
 console.log(mincost([1, 2, 3, 4, 5])); // 33
-
+console.log(mincost([5])); // 0 (only one rope)
