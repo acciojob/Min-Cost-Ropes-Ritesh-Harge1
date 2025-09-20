52d49c91fc50c5911beadd53484f3a13b3b00486
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-09-20 18:55:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,79 +1,42 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  // Get index helpers
-  getParent(i) { return Math.floor((i - 1) / 2); }
-  getLeft(i) { return 2 * i + 1; }
-  getRight(i) { return 2 * i + 2; }
-
-  // Insert into heap
-  insert(val) {
-    this.heap.push(val);
-    this.heapifyUp();
-  }
-
-  // Heapify upwards
-  heapifyUp() {
-    let i = this.heap.length - 1;
-    while (i > 0 && this.heap[this.getParent(i)] > this.heap[i]) {
-      [this.heap[i], this.heap[this.getParent(i)]] =
-        [this.heap[this.getParent(i)], this.heap[i]];
-      i = this.getParent(i);
-    }
-  }
-
-  // Extract min element
-  extractMin() {
-    if (this.heap.length === 0) return null;
-    if (this.heap.length === 1) return this.heap.pop();
-
-    let root = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.heapifyDown(0);
-    return root;
-  }
-
-  // Heapify downwards
-  heapifyDown(i) {
-    let smallest = i;
-    let left = this.getLeft(i);
-    let right = this.getRight(i);
-
-    if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
-      smallest = left;
-    }
-    if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
-      smallest = right;
-    }
-    if (smallest !== i) {
-      [this.heap[i], this.heap[smallest]] =
-        [this.heap[smallest], this.heap[i]];
-      this.heapifyDown(smallest);
-    }
-  }
-}
-
-// Min cost ropes function
+// Function to calculate minimum cost of connecting ropes
 function mincost(arr) {
-  let heap = new MinHeap();
-  for (let num of arr) heap.insert(num);
+  // Use built-in array sort + shift for simplicity
+  // (for N <= 1000, this is efficient enough)
+  arr.sort((a, b) => a - b);
 
   let totalCost = 0;
 
-  while (heap.heap.length > 1) {
-    let first = heap.extractMin();
-    let second = heap.extractMin();
+  while (arr.length > 1) {
+    // Take two smallest
+    let first = arr.shift();
+    let second = arr.shift();
+
     let cost = first + second;
     totalCost += cost;
-    heap.insert(cost);
+
+    // Insert back into sorted position
+    let i = 0;
+    while (i < arr.length && arr[i] < cost) i++;
+    arr.splice(i, 0, cost);
   }
 
   return totalCost;
 }
 
-// Example
-console.log(mincost([4, 3, 2, 6])); // 29
-console.log(mincost([1, 2, 3, 4, 5])); // 33
+// Main logic for Node.js input
+function main(input) {
+  const lines = input.trim().split("\n");
+  const arr = lines[0].replace(/\[|\]/g, "").split(",").map(Number);
+
+  console.log(mincost(arr));
+}
+
+// Read input from stdin
+let inputData = "";
+process.stdin.on("data", (chunk) => {
+  inputData += chunk;
+});
+process.stdin.on("end", () => {
+  main(inputData);
+});
 
